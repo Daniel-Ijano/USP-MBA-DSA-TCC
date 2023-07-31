c54ca86769117b5c1ae00b9b240167e024421c38
Daniel Ijano <daniel.ijano@gmail.com>
2023-07-31 20:54:51 -0300

    Petlove (WIP)

--- EcommerceOffers/petlove/petlove/petlove/spiders/petlove.py
+++ EcommerceOffers/petlove/petlove/petlove/spiders/petlove.py
@@ -45,8 +45,9 @@ class PetloveSpider(scrapy.Spider):
         start_urls = []
         for specie in species:
             for category in categories:
-                link=self.CATEGORIES_URL(specie=specie, category=category, page=str(page))
-                start_urls.append(link)
+                cat_url = self.CATEGORIES_URL(specie=specie, category=category, page=str(page))
+                url_meta = {"url": cat_url, "specie": specie, "category": category, "page": str(page)}
+                start_urls.append(url_meta)
 
         # Create CloudScraper Instance -> Chrome browser // Windows OS // Desktop
         scraper = cloudscraper.create_scraper(
@@ -58,13 +59,13 @@ class PetloveSpider(scrapy.Spider):
             }
         )
 
-        for url in start_urls:
-            response = scraper.get(url, headers=self.HEADERS)
+        for url in start_urls[:1]:
+            response = scraper.get(url["url"], headers=self.HEADERS)
             yield scrapy.Request(
                 url=self.DUMMY_URL,
                 callback=self.parse,
                 dont_filter=True,
-                meta={'response': response, 'specie': specie, 'category': category, 'page': page}
+                meta={'response': response, 'specie': url["specie"], 'category': url["category"], 'page': url["page"]}
             )
 
     def parse(self, response):
@@ -85,11 +86,11 @@ class PetloveSpider(scrapy.Spider):
             print("\n","\n", ">>>>> No offer found: ", response.meta.get('response').url, "\n","\n")
             return
 
-        for offer in offers_list:
+        for offer in offers_list[:1]:
             items['collected_at'] = datetime.today().strftime('%Y-%m-%d')
             items['source'] = "Petlove"
-            items['specie'] = specie.capitalize() ######################### BROKEN
-            items['category'] = category.split("/")[1].replace("-", " ").title() ######################### BROKEN
+            items['specie'] = specie.capitalize()
+            items['category'] = category.split("/")[1].replace("-", " ").title()
             items['brand'] = offer.get("marca", "")
             items['rating'] = float(offer.get("avaliacao", 0))
             items['sku'] = offer.get("sku", "")
@@ -102,25 +103,73 @@ class PetloveSpider(scrapy.Spider):
             items['title'] = offer.get("nome", "")
             items['description'] = items['title']
 
-            # offer['details']
-            # offer['extras']
+            variants = []
             skus = offer.get("extras_skus", [])
             for sku in skus:
-                if sku.get("sku", "") == items['sku']:
-                    items['status'] = sku.get("properties", "").get("status", "").capitalize()
+                variant_sku = sku.get("sku", "")
+                pkg_size = sku.get("properties", "").get("spec", "")
+                status = sku.get("properties", "").get("status", "").capitalize()
+                
+                # Get the status of the current sku
+                if variant_sku == items['sku']:
+                    items['status'] = status
+
+                # Store the variants sku for future use
+                elif status == "Available":
+                    variant = {"variant_sku": variant_sku, "pkg_size": pkg_size, "status": status}
+                    variants.append(variant)
 
             # Send items
             yield items
 
-            # Se a offerta tiver multiplas skus
+            # Create CloudScraper Instance -> Chrome browser // Windows OS // Desktop
+            scraper = cloudscraper.create_scraper(
+                browser={
+                    'browser': 'chrome',
+                    'platform': 'ios',
+                    'desktop': False,
+                    'mobile': True,
+                }
+            )
+
+            # Offer request for variants
             #if offer.get("multiple_skus", ""):
-            # if offer.get("hasMultipleVariants", ""):
-            #     items = PetloveItem()
-            #     #offer.get("link", "") + "?sku=" + items['sku'] #OK
-            #     pass
+            if offer.get("hasMultipleVariants", "") or variants:
+                for variant in variants:
+                    url = offer.get("link", "") + "?sku=" + variant["variant_sku"]
+                    response = scraper.get(url, headers=self.HEADERS)
+                    metadados = {"variant": variant, "items": items}
+                    yield from self.parse_offer(response, metadados)
 
         # Pagination
-        # next_page = page + 1
-        # url = self.CATEGORIES_API(specie=specie, category=category, page=next_page)
-        # meta={'specie': specie, 'category': category, 'page': next_page}
-        # yield response.follow(url, callback=self.parse, meta=meta)
+        next_page = page + 1
+        url = self.CATEGORIES_URL(specie=specie, category=category, page=str(next_page))
+        response = scraper.get(url, headers=self.HEADERS)
+        yield scrapy.Request(
+            url=self.DUMMY_URL,
+            callback=self.parse,
+            dont_filter=True,
+            meta={'response': response, 'specie': specie, 'category': category, 'page': next_page}
+        )
+
+    def parse_offer(self, response, metadados):
+        # Getting variants data
+        parsed_response = html.fromstring(response.content.decode("utf-8"))
+
+        items = PetloveItem()
+        items = metadados["items"]
+        items["url"] = response.url
+        items["sku"] = metadados["variant"]["variant_sku"]
+        items["pkg_size"] = metadados["variant"]["pkg_size"]
+
+        title = parsed_response.xpath("//title/text()")[0].replace(" | Petlove", "")
+        items["title"] =  f"{title} - {items['pkg_size']}"
+        items["description"] = items["title"]
+        import ipdb; ipdb.set_trace()
+
+        # Xpaths #################################################################################################### FALTA AQUI E DEPOIS MELHORAR A DUMMY REQUEST
+        items["regular_price"] = None
+        items["sub_price"] = None
+
+        # Send items
+        # yield items
